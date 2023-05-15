ee11c42f07662590dcfc03fcbca2503f48109b30
ankitajaswal <ankitajaswal508@gmail.com>
2023-05-14 21:24:39 -0700

    update calender component.2

--- frontend/wiors/src/CalenderComponent.js
+++ frontend/wiors/src/CalenderComponent.js
@@ -6,13 +6,18 @@ function CalendarComponent() {
   const [date, setDate] = useState(new Date());
 
   return (
-    <div className='calendar-container'>
+    <>
+    <div className='calendar-container' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <Calendar onChange={setDate} value={date} />
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
+    </div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <p className='text-center' >
+        <span className='bold' >Selected Date:</span>
+        {' '}
         {date.toDateString()}
       </p>
-    </div>
+      </div>
+      </>
   );
 }
 
