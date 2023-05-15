1eacaea744f083b77ef27050a6f61b17a7bb314b
ankitajaswal <ankitajaswal508@gmail.com>
2023-05-14 23:05:27 -0700

    simple navbar with login and register components

--- frontend/wiors/src/App.js
+++ frontend/wiors/src/App.js
@@ -1,12 +1,29 @@
-import CalendarComponent from './CalenderComponent.js';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBarComponent from './components/NavBarComponent';
+import HomeComponent from './components/HomeComponent';
+import CalendarComponent from './components/CalendarComponent';
+import Register from './components/RegisterComponent';
+import Login from './components/LoginComponent';
 
 function App() {
   return (
-    <div className='app'>
-      <h1 className='text-center'>Seat Reserve</h1>
-      <CalendarComponent />
-    </div>
+    <Router>
+      <div className='app'>
+      <NavBarComponent />
+
+      <Routes>        
+          <Route exact path='/' element={<HomeComponent />} />
+          < Route path='/calendar' element={<CalendarComponent />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+      </Routes>
+
+      </div>
+
+    </Router>
   );
 }
 
 export default App;
+
---
+++ frontend/wiors/src/components/LoginComponent.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('employee');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${username}\nPassword: ${password}\nRole: ${role}`);
+    // handle form submission here
+  };
+
+  return (
+    <div className='login-form'>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username:</label>
+        <input
+          type='text'
+          id='username'
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <label htmlFor='password'>Password:</label>
+        <input
+          type='password'
+          id='password'
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <label htmlFor='role'>Role:</label>
+        <select id='role' value={role} onChange={(event) => setRole(event.target.value)}>
+          <option value='employee'>Employee</option>
+          <option value='employer'>Employer</option>
+        </select>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
