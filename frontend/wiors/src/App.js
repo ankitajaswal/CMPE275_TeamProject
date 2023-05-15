db1a7293497125b3e81ac9dc4a4b3c495b4c8801
ankitajaswal <ankitajaswal508@gmail.com>
2023-05-14 20:30:36 -0700

    calender component added

--- frontend/wiors/src/App.js
+++ frontend/wiors/src/App.js
@@ -1,20 +1,10 @@
-import { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import CalendarComponent from './CalenderComponent.js';
 
 function App() {
-  const [date, setDate] = useState(new Date());
-
   return (
     <div className='app'>
       <h1 className='text-center'>Seat Reserve</h1>
-      <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} />
-      </div>
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
-        {date.toDateString()}
-      </p>
+      <CalendarComponent />
     </div>
   );
 }
