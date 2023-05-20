ed538257d9d8873b4ed6c64e41d7ce8f1f10f5b3
ankitajaswal <ankitajaswal508@gmail.com>
2023-05-19 22:52:56 -0700

    added start and end date

--- frontend/wiors/src/components/SeatReservationComponent.js
+++ frontend/wiors/src/components/SeatReservationComponent.js
@@ -1,55 +1,103 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-//import 'react-calendar/dist/Calendar.css';
+import 'react-calendar/dist/Calendar.css';
 import '../styles/seatreservation.css';
+
 function SeatReservation() {
+  const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const [selectedEndDate, setSelectedEndDate] = useState(null);
   const [selectedDates, setSelectedDates] = useState([]);
 
   const handleDateChange = (date) => {
-    const dayOfWeek = date.getDay();
-
-    // Allow selection only on Monday to Friday (0 = Sunday, 6 = Saturday)
-    if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-      const selectedDateIndex = selectedDates.findIndex((selectedDate) =>
-        selectedDate.toDateString() === date.toDateString()
-      );
-
-      if (selectedDateIndex > -1) {
-        const updatedDates = [...selectedDates];
-        updatedDates.splice(selectedDateIndex, 1);
-        setSelectedDates(updatedDates);
-      } else {
-        setSelectedDates([...selectedDates, date]);
+    if (!selectedStartDate) {
+      if (date.getDay() >= 1 && date.getDay() <= 5) {
+        setSelectedStartDate(date);
+        setSelectedEndDate(null);
+        setSelectedDates([]);
       }
+    } else if (!selectedEndDate && date > selectedStartDate) {
+      const startDate = new Date(selectedStartDate);
+      const endDate = new Date(date);
+      /*When selecting the end date, 
+      it checks if the selected date is a weekday and greater than the start date. 
+      It calculates the difference in days between the start and end dates 
+      using the (endDate - startDate) / (1000 * 60 * 60 * 24) formula. 
+      If the selected date satisfies these conditions 
+      and the difference is 4 days or less, it is set as the end date, 
+      and the dates within the range are updated.
+      */
+      const daysDifference = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
+      //When selecting the start date, 
+      //it verifies if the selected date is a weekday (Monday to Friday) using the getDay() method. 
+      //If it is, the date is set as the start date.
+      if (date.getDay() >= 1 && date.getDay() <= 5 && daysDifference <= 4) {
+        setSelectedEndDate(date);
+        const datesInRange = getDatesInRange(selectedStartDate, date);
+        setSelectedDates(datesInRange);
+      }
+    } else {
+      setSelectedStartDate(date);
+      setSelectedEndDate(null);
+      setSelectedDates([]);
     }
+  };
+  
+  const getDatesInRange = (startDate, endDate) => {
+    const dates = [];
+    const currentDate = new Date(startDate);
+    while (currentDate <= endDate) {
+      const dayOfWeek = currentDate.getDay();
+      if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+        dates.push(new Date(currentDate));
+      }
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    return dates;
   };
 
   const highlightSelectedDates = ({ date }) => {
     const dayOfWeek = date.getDay();
-  
-    // Check if the date is included in the selectedDates array
-    const isSelected = selectedDates.some((selectedDate) => selectedDate.toDateString() === date.toDateString());
-  
-    // Highlight selected dates (Monday to Friday) with a blue background color
-    if (dayOfWeek >= 1 && dayOfWeek <= 5 && isSelected) {
+    const isStartSelected = selectedStartDate && date.toDateString() === selectedStartDate.toDateString();
+    const isEndSelected = selectedEndDate && date.toDateString() === selectedEndDate.toDateString();
+
+    // Highlight selected start date and end date
+    if (isStartSelected || isEndSelected) {
       return (
         <div className="highlighted-date">
           {date.getDate()}
         </div>
       );
     }
-  
+
+    // Highlight selected dates within the range
+    if (selectedStartDate && selectedEndDate && dayOfWeek >= 1 && dayOfWeek <= 5) {
+      if (date >= selectedStartDate && date <= selectedEndDate) {
+        return (
+          <div className="highlighted-date">
+            {date.getDate()}
+          </div>
+        );
+      }
+    }
+
     return null;
   };
-  
-  
 
   return (
     <>
       <div className='calendar-container' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Calendar onChange={handleDateChange} value={selectedDates} tileContent={highlightSelectedDates} />
+        <Calendar onChange={handleDateChange} value={selectedStartDate || selectedEndDate} tileContent={highlightSelectedDates} />
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '1rem' }}>
+        {selectedStartDate && selectedEndDate && (
+          <p className='text-center'>
+            <span className='bold'>Start Date:</span> {selectedStartDate.toDateString()}
+            <br />
+            <span className='bold'>End Date:</span> {selectedEndDate.toDateString()}
+          </p>
+        )}
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '1rem' }}>
         <p className='text-center'>
           <span className='bold'>Selected Dates:</span>
           {' '}
