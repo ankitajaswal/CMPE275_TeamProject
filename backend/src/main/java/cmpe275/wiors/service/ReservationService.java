04f8683436905cf1544b2d63aff0de2c7383e61e
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2023-05-23 02:22:54 -0700

    Updated reservation service - fix reserved seat bug

--- backend/src/main/java/cmpe275/wiors/service/ReservationService.java
+++ backend/src/main/java/cmpe275/wiors/service/ReservationService.java
@@ -1,18 +1,24 @@
 package cmpe275.wiors.service;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cmpe275.wiors.entity.Employee;
 import cmpe275.wiors.entity.Reservation;
+import cmpe275.wiors.entity.Seat;
 import cmpe275.wiors.repository.ReservationRepository;
+import cmpe275.wiors.repository.SeatRepository;
 
 @Service
 public class ReservationService {
     
     @Autowired
     private ReservationRepository reservationRepo;
+    @Autowired 
+    private SeatRepository seatRepo;
 
     /**
      * persist a reservation
@@ -59,4 +65,42 @@ public class ReservationService {
     public void deleteAllReservations(String employerId) {
         reservationRepo.deleteAllByEmployerId(employerId);
     }
+
+
+    /**
+     * gets all seats not reserved on a date 
+     * @param employerId
+     * @param date
+     * @return list of vacant seats 
+     */
+    public List<Seat> getVacantSeats( String employerId, Date date) {
+        List<Seat> seats = seatRepo.findByEmployerId(employerId);
+        List<Reservation> reservations = reservationRepo.findByEmployerIdAndDate(employerId, date);
+        for (Reservation reservation : reservations) {
+            Seat s = reservation.getSeat();
+            seats.remove(s);            
+        }
+        return seats;
+    }
+
+    
+    /**
+     * Check to see if employee has already reserved a seat for this day
+     * @param employerId
+     * @param employeeId
+     * @param date
+     * @return
+     */
+    public boolean checkReservationByEmployee(String employerId, Long employeeId, Date date ) {
+        List<Reservation> reservations = reservationRepo.findByEmployerIdAndDate(employerId, date);
+        for (Reservation reservation : reservations) {
+            Employee e = reservation.getReservee();
+            Long eId = e.getId();
+            if (eId == employeeId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
