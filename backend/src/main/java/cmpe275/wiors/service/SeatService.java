e11a6f2e858398a440002fd4b96d9767524d7d14
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2023-05-23 02:19:06 -0700

    updated SeatService - fixed reserved seat bug

--- backend/src/main/java/cmpe275/wiors/service/SeatService.java
+++ backend/src/main/java/cmpe275/wiors/service/SeatService.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import cmpe275.wiors.entity.Seat;
-import cmpe275.wiors.entity.Employee;
-import cmpe275.wiors.repository.EmployeeRepository;
 import cmpe275.wiors.repository.SeatRepository;
 
 @Service
@@ -15,8 +13,6 @@ public class SeatService {
     @Autowired
     private SeatRepository seatRepo;
 
-    @Autowired
-    private EmployeeRepository employeeRepo;
 
     /**
      * Persist a seat
@@ -38,17 +34,6 @@ public class SeatService {
     }
 
     /**
-     * 
-     * @param seatOwner Employer which owns the seats
-     * @param status seat status (is reserved)
-     * @return list of seats with specified status for employer
-     */
-    public List<Seat> getAllSeatsByEmployerAndStatus(String ownerId, boolean status) {
-        return seatRepo.findByEmployerIdAndReserved(ownerId, status);
-    }
-
-
-    /**
      * get a single seat
      * @param ownerId
      * @param seatId
@@ -56,38 +41,6 @@ public class SeatService {
      */
     public Seat getSeatByEmployerAndId(String ownerId, Long seatId) {
         return seatRepo.findByEmployerIdAndId(ownerId, seatId);
-    }
-
-
-    /**
-     * update the seat status to vacant
-     * @param ownerId
-     * @param seatId
-     * @return single seat object info
-     */
-    public Seat updateSeatStatusVacant(String ownerId, Long seatId) {
-        Seat s = seatRepo.findByEmployerIdAndId(ownerId, seatId);
-        s.setResevrved(false);
-        s.setReservee(null);
-        return seatRepo.save(s);
-    }
-
-
-    /**
-     * update seat status to reserved
-     * Populate the reservee field
-     * @param ownerId
-     * @param seatId
-     * @param employeeId
-     * @param status
-     * @return updated seat object for that seatId
-     */
-    public Seat updateSeatStatusReserved(String ownerId, Long seatId, Long employeeId) {
-        Seat s = seatRepo.findByEmployerIdAndId(ownerId, seatId);
-        s.setResevrved(true);
-        Employee reservee = employeeRepo.findById(employeeId).get();
-        s.setReservee(reservee);
-        return seatRepo.save(s);
     }
 
 
