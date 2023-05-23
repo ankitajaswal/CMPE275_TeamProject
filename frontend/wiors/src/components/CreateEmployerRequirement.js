834d084ae68cf9a321756a64df903e3a532ec0ae
Aidan Jones <arjones239@protonmail.ch>
2023-05-22 23:49:39 -0700

    added update employer req

--- frontend/wiors/src/components/CreateEmployerRequirement.js
+++ frontend/wiors/src/components/CreateEmployerRequirement.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import EmployerRequirement from './EmployerRequirement';
 
 class CreateEmployerRequirement extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { mop: 0 };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleMopChange = this.handleMopChange.bind(this);
+    }
+
+    handleMopChange(event) {
+        this.setState({ mop: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let url = global.config.url + "requirement/employer/" 
+            + global.config.employerId + "?numberOfDays=" + this.state.mop;
+
+        let iterator = fetch(url, {
+            method: "POST",
+        });
+        iterator.then(res => {});
+        // EmployerRequirement.componentDidMount();
+    }
+
     render() {
+        const mop = this.state.mop;
+
         return (
-            <div>placeholder</div>
+            <div>
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor='mop'>Enter new MOP:</label>
+                <input type='number' id='mop' value={mop} onChange={this.handleMopChange} />
+                <button type='submit'>Submit</button>
+            </form>
+            </div>
         )
     }
 
