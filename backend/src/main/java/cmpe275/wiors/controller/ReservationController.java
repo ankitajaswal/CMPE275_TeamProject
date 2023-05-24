698991aae3092bdad5124ca3fdb402ecf04473f2
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2023-05-23 20:17:55 -0700

    updated reservation controller for bulk support

--- backend/src/main/java/cmpe275/wiors/controller/ReservationController.java
+++ backend/src/main/java/cmpe275/wiors/controller/ReservationController.java
@@ -1,20 +1,28 @@
 package cmpe275.wiors.controller;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.time.DayOfWeek;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import cmpe275.wiors.entity.BulkReservation;
 import cmpe275.wiors.entity.Employee;
 import cmpe275.wiors.entity.Employer;
 import cmpe275.wiors.entity.Reservation;
@@ -84,6 +92,58 @@ public class ReservationController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         reservationService.createReservation(new Reservation(employer, reservee, reservedSeat, date));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @Transactional
+    @RequestMapping(
+        value = "/bulkReservation/{employerId}",
+        method = RequestMethod.POST,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> bulkReservation(
+        @Valid @RequestBody List<BulkReservation> br, 
+        @PathVariable String employerId
+    ) throws ParseException {
+        for (BulkReservation bulkReservation : br) {
+            String reserveeEmail = bulkReservation.getEmail();
+            Employee reservee = employeeService.getEmployeeByEmail(reserveeEmail);
+            Long reserveeId = reservee.getId();
+            String startDateString = bulkReservation.getStartDate();
+            String endDateString = bulkReservation.getEndDate();
+
+            Date startDate = null;
+            Date endDate = null;
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+            try {
+                java.util.Date utilDateStart = dateFormat.parse(startDateString);
+                java.util.Date utilDateEnd = dateFormat.parse(endDateString);
+                startDate = new java.sql.Date(utilDateStart.getTime());
+                endDate = new java.sql.Date(utilDateEnd.getTime());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            
+            Calendar calendar = Calendar.getInstance();
+            
+            calendar.setTime(startDate);
+            int startDay = calendar.get(Calendar.DAY_OF_WEEK);
+        
+            calendar.setTime(endDate);
+            int endDay = calendar.get(Calendar.DAY_OF_WEEK);
+            List<Date> datesOfWeek = new ArrayList<>();
+
+            calendar.set(Calendar.DAY_OF_WEEK, startDay);
+
+            // forloop  - add check for not sat/sun and start < end (start date may = end)
+            for (int i = startDay; i <= endDay; i++) {
+                Date resultDate = new Date(calendar.getTimeInMillis());
+                datesOfWeek.add(resultDate);
+                calendar.add(Calendar.DAY_OF_WEEK, 1);
+                this.createReservation(employerId, reserveeId, resultDate);
+            }
+        }
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
