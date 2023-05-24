2a053c02511d5248d85e499ddd0b8caf2ac65c17
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2023-05-23 23:36:31 -0700

    committed App.js fe

--- frontend/wiors/src/App.js
+++ frontend/wiors/src/App.js
@@ -7,6 +7,7 @@ import Login from './components/LoginComponent';
 import CSVImporterComponent from './components/CSVImporterComponent';
 import SeatReservation from './components/SeatReservationComponent';
 import AttendanceRequirements from './components/AttendanceRequirements';
+import UserDash from './components/UserDash';
 
 function App() {
   return (
@@ -21,7 +22,7 @@ function App() {
           <Route path='/seatreservation' element={<SeatReservation />} />
           <Route path='/bulk' element={<CSVImporterComponent />} />
           <Route path='/attendancerequirements' element={<AttendanceRequirements />} />
-
+          <Route path='/userDash' element={<UserDash />} />
       </Routes>
 
       </div>
